ecadd9503f1fa8eb83a482ff9e9f3418fe2a409c
NiekDoo <tobiastevelde@gmail.com>
2025-01-31 15:59:22 +0100

    get nearest shit

--- Functionality/Scripts/bullet.gd
+++ Functionality/Scripts/bullet.gd
@@ -4,9 +4,22 @@ var enemy_array: Array
 var shot: bool = true #i made it true to make the code runnable for now
 var enemy
 var direction
+var nearest_enemy = null
+
 
 func _process(delta: float) -> void:
 	enemy_array = get_tree().get_nodes_in_group("enemies") #dit is denk ik beter dan: = []
+	if enemy_array.size() > 0:
+		if nearest_enemy == null:
+			nearest_enemy = enemy_array[0]
+		for i in enemy_array:
+			var temp_distance = position.distance_to(i.position)
+			if temp_distance < position.distance_to(nearest_enemy.position):
+				nearest_enemy = i
+		
+		if nearest_enemy != null:
+			self.position = nearest_enemy.position
+	
 	if shot == false:
 		direction = Global.player_position - Global.enemy_position
 
