6400e28e85f044e1843f96a67f0fdc7c70c228a5
matthigame <matthiasnijman08@gmail.com>
2025-02-07 15:53:22 +0100

    commit

---
+++ Assets/Scripts/GameData.gd
@@ -0,0 +1,48 @@
+extends Node
+var Spells = {
+	0: {
+		"Name" = "Magic missile",
+		"Damage" = 30,
+	},
+	1: {
+		"Name" = "Divine protection",
+		"Damage" = 0,
+	},
+	2: {
+		"Name" = "Arcane shove",
+		"Damage" = 5,
+	},
+	3: {
+		"Name" = "Arcane construct",
+		"Damage" = 0,
+	},
+	4: {
+		"Name" = "Summon golem",
+		"Damage" = 0,
+	},
+}
+var Items = {
+	0: {
+		"Name" = "Healing Balm",
+		"Damage" = 30,
+	},
+	1: {
+		"Name" = "Hawk's sight",
+		"Damage" = 0,
+	},
+	2: {
+		"Name" = "Panther's dexterity",
+		"Damage" = 5,
+	},
+	3: {
+		"Name" = "Armadillo's resistance",
+		"Damage" = 0,
+	},
+	4: {
+		"Name" = "Hummingbird's haste",
+		"Damage" = 0,
+	},
+	5: {
+		"Name" = "Gorilla's brawn"
+	}
+}
---
+++ Assets/Scripts/druid_menu.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_4_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_5_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_6_pressed() -> void:
+	pass # Replace with function body.
