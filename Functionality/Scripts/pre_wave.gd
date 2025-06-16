ddcf5a0aa912a7cf7a5000727da8d8eb6e34fab2
matthigame <matthiasnijman08@gmail.com>
2025-06-16 15:11:14 +0200

    commit

--- Functionality/Scripts/pre_wave.gd
+++ Functionality/Scripts/pre_wave.gd
@@ -1,6 +1,8 @@
 extends Node2D
 var redraw = false
 var x = 0
+var penalty = 0
+var count = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in Global.PlayerSpells.size():
@@ -13,6 +15,14 @@ func _ready():
 		add_child(button)
 		button.pressed.connect(func(): _on_spell_pressed(button.text))
 		kys(button)
+		if Global.PlayerSpells[i][3] == true:
+			_make_label(button.position, button.size, 255, "\n active")
+		else:
+			_make_label(button.position, button.size, 255, "\n inactive")
+	if _count() >= 3:
+		penalty = (_count() - 2) * 0.05
+	$ColorRect/Button.text = "Continue \n penalty = " + str(penalty)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -21,6 +31,7 @@ func _process(delta):
 
 func _on_button_pressed():
 	Global.wave_start.emit(Global.current_wave)
+	Global.player_damage_mult -= penalty
 	queue_free()
 
 func _on_spell_pressed(name):
@@ -30,13 +41,16 @@ func _on_spell_pressed(name):
 				Global.PlayerSpells[i][3] = false
 			else:
 				Global.PlayerSpells[i][3] = true
+	if _count() >= 3:
+		penalty = (_count() - 2) * 0.05
+	_on_h_scroll_bar_value_changed($ColorRect/HScrollBar.value)
 
 func kys(button):
+	x += 1
+	if x >= Global.PlayerSpells.size() - 1:
+		redraw = false
 	while redraw == false:
 		await get_tree().process_frame
-	x += 1
-	if x >= Global.PlayerSpells.size():
-		redraw = false
 	button.queue_free()
 
 func _on_h_scroll_bar_value_changed(value):
@@ -44,14 +58,39 @@ func _on_h_scroll_bar_value_changed(value):
 	await get_tree().process_frame
 	for i in Global.PlayerSpells.size():
 		var button = Button.new()
-		button.text = GameData.Spells[Global.PlayerSpells[i][0]]["Name"]
+		button.text = GameData.Spells[Global.PlayerSpells[i][0]]["Name"] 
 		button.position.y = 150
 		button.position.x = 160 + i * 100 - $ColorRect/HScrollBar.value * 2
 		button.size.x = 75
 		button.size.y = 30
-		button.self_modulate.a8 = 255 - $ColorRect/HScrollBar.value * 8
+		if button.position.x < 160:
+			button.self_modulate.a8 = 255 + i * 335 - $ColorRect/HScrollBar.value * 8
+		else:
+			button.self_modulate.a8 = 255
 		add_child(button)
 		button.pressed.connect(func(): _on_spell_pressed(button.text))
 		kys(button)
 		if button.position.x < 90:
 			button.disabled = true
+		if Global.PlayerSpells[i][3] == true:
+			_make_label(button.position, button.size, button.self_modulate.a8, "\n active")
+		else:
+			_make_label(button.position, button.size, button.self_modulate.a8, "\n inactive")
+		$ColorRect/Button.text = "Continue \n penalty = " + str(penalty)
+
+func _make_label(position, size, modulation, active):
+	redraw = true
+	var label = Label.new()
+	label.text = active
+	label.position = position
+	label.size = size
+	label.self_modulate.a8 = modulation
+	add_child(label)
+	kys(label)
+
+func _count():
+	count = 0
+	for i in Global.PlayerSpells.size():
+		if Global.PlayerSpells[i][3] == true:
+			count += 1
+	return count
